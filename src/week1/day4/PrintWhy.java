4d58f54e26e4bb895f03490c20b89217a500f153
KokoaTree <4kakao1@daum.net>
2023-04-20 11:22:38 +0900

    feat: day4 Java 프로젝트 - 문자 출력(print) - 문자 출력(printf)

---
+++ src/week1/day4/PrintWhy.java
@@ -0,0 +1,13 @@
+package week1.day4;
+
+public class PrintWhy {
+    void print() {
+        System.out.println("Why");
+    }
+
+    public static void main(String[] args) {
+        PrintWhy why = new PrintWhy();
+        why.print();
+
+    }
+}
