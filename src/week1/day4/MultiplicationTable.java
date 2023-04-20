144e123ddce965ed924df58b429c5d87ba6404fc
KokoaTree <4kakao1@daum.net>
2023-04-20 11:33:49 +0900

    feat: day4 PrintfTwoValuesShortage, MultiplicationTable 작성 - 문장 출력 - 곱셈 출력

---
+++ src/week1/day4/MultiplicationTable.java
@@ -0,0 +1,9 @@
+package week1.day4;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println("2" + "*" + "1" + "=" + "2");
+        System.out.printf("%d * %d = %d\n", 2, 2, 4);
+        System.out.printf("%d * %d = %d", 2, 3, 2 * 3);
+    }
+}
---
+++ src/week1/day4/PrintfTwoValuesShortage.java
@@ -0,0 +1,7 @@
+package week1.day4;
+
+public class PrintfTwoValuesShortage {
+    public static void main(String[] args){
+        System.out.printf("%s님 안녕하세요. 그런데 제가 하고 싶은 말은 %s입니다.", "김경록", "김태환");
+    }
+}
