d7109af41af2471b314f127a2b15ec659e4945cc
KokoaTree <4kakao1@daum.net>
2023-04-20 13:18:04 +0900

    feat: day4 Multiple2 - 구구단 2단 출력

---
+++ src/week1/day4/Multiple2.java
@@ -0,0 +1,16 @@
+package week1.day4;
+
+public class Multiple2 {
+    public static void main(String[] args) {
+        System.out.printf("%d X %d = %2d\n", 2, 1, 2 * 1);
+        System.out.printf("%d X %d = %2d\n", 2, 2, 2 * 2);
+        System.out.printf("%d X %d = %2d\n", 2, 3, 2 * 3);
+        System.out.printf("%d X %d = %2d\n", 2, 4, 2 * 4);
+        System.out.printf("%d X %d = %2d\n", 2, 5, 2 * 5);
+        System.out.printf("%d X %d = %2d\n", 2, 6, 2 * 6);
+        System.out.printf("%d X %d = %2d\n", 2, 7, 2 * 7);
+        System.out.printf("%d X %d = %2d\n", 2, 8, 2 * 8);
+        System.out.printf("%d X %d = %2d\n", 2, 9, 2 * 9);
+    }
+
+}
