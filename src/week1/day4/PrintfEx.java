ddaea4c90641e10ba75caf329ed3e7e745dbbed9
KokoaTree <4kakao1@daum.net>
2023-04-20 11:22:22 +0900

    feat: day4 Java 프로젝트 - 문자 출력(print) - 문자 출력(printf)

---
+++ src/week1/day4/PrintfEx.java
@@ -0,0 +1,12 @@
+package week1.day4;
+
+public class PrintfEx {
+    public void print() {
+        System.out.printf("%s", "Hello\n");
+        System.out.println("bye");
+    }
+    public static void main(String[] args) {
+        PrintfEx printfEx = new PrintfEx();
+        printfEx.print();
+    }
+}
