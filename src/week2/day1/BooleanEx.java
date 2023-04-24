e1c5e5ac528245d97a439beacb775e961df0f1b2
KokoaTree <4kakao1@daum.net>
2023-04-24 14:38:55 +0900

    feat: BooleanEx - Boolean 변수 선언 실습

---
+++ src/week2/day1/BooleanEx.java
@@ -0,0 +1,17 @@
+package week2.day1;
+
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean flag1 = true;
+        boolean flag2 = false;
+
+        boolean isPaymentSuccess = false;
+        boolean isBalanceSufficient = true;
+
+        System.out.println(flag1);
+        System.out.println(flag2);
+        System.out.println(isPaymentSuccess);
+        System.out.println(isBalanceSufficient);
+
+    }
+}
