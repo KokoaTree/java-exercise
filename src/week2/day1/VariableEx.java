f82ee7a7ec39b63f49a8849fbf1fdd2f33659e64
KokoaTree <4kakao1@daum.net>
2023-04-24 09:16:11 +0900

    feat: VariableEx - 변수 타입별 출력

---
+++ src/week2/day1/VariableEx.java
@@ -0,0 +1,13 @@
+package week2.day1;
+
+public class VariableEx {
+    public static void main(String[] args) {
+
+        int iOne = 1;
+        float fOne = 1.0f;
+        String sONe = new String("1");
+
+        System.out.printf("String: %s, int: %d, float: %f", sONe, iOne, fOne);
+    }
+
+}
