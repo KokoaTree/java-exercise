318bff2b479b4772e078582e53e8507e42b23ce2
KokoaTree <4kakao1@daum.net>
2023-04-24 11:30:01 +0900

    feat: FloatAndDouble, FloatDigit - float 결과값 범위 실습 - double 결과값 범위 실습 -float, double 소수점 출력

---
+++ src/week2/day1/FloatDigit.java
@@ -0,0 +1,20 @@
+package week2.day1;
+
+public class FloatDigit {
+    public static void main(String[] args) {
+        float f5 = 1.23e5f;
+        float f6 = 1.23e6f;
+        float f7 = 1.23e7f;
+        float f8 = 1.23e8f;
+        float f9 = 1.23e9f;
+        float f10 = 1.23e10f;
+
+        System.out.println("f5: %f\n" + f5);
+        System.out.println("f6: %f\n" + f6);
+        System.out.println("f7: %f\n" + f7);
+        System.out.println("f8: %f\n" + f8);
+        System.out.println("f9: %f\n" + f9);
+        System.out.println("f10: %f\n" + f10);
+
+    }
+}
