8dc179f9e6a1f79aacfb7b4e11552eb242b3742d
KokoaTree <4kakao1@daum.net>
2023-04-26 15:39:01 +0900

    feat: 변수 값 누적하기 - LikeAccumulate 실습 - AccountAccumulate 실습

---
+++ src/week2/day3/AccountAccumulate.java
@@ -0,0 +1,12 @@
+package week2.day3;
+
+public class AccountAccumulate {
+    public static void main(String[] args) {
+        int myAccount = 1_000_000;
+        myAccount = myAccount - 500_000;
+        System.out.println(myAccount);
+
+        myAccount = myAccount + 4_000_000;
+        System.out.println(myAccount);
+    }
+}
