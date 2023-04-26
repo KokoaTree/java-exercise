496f88ea6193ca93fbf03aee199619313e7f5b40
KokoaTree <4kakao1@daum.net>
2023-04-26 11:35:38 +0900

    feat: 2차원 배열(메소드포함) - TwoDimArray 추가 실습

--- src/week2/day3/TwoDimArray.java
+++ src/week2/day3/TwoDimArray.java
@@ -3,8 +3,16 @@ package week2.day3;
 import java.util.Arrays;
 
 public class TwoDimArray {
+    public static void printArray(int[][] arr){
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+        System.out.printf("-----------------\n");
+    }
     public static void main(String[] args) {
         int[][] arr = new int[3][3];
+        printArray(arr);
+
         int[] arr0 = arr[0];
         arr0[0] = 1;
 
@@ -14,8 +22,17 @@ public class TwoDimArray {
         int[] arr2 = arr[2];
         arr2[2] = 1;
 
-        System.out.println(Arrays.toString(arr[0]));
-        System.out.println(Arrays.toString(arr[1]));
-        System.out.println(Arrays.toString(arr[2]));
+        printArray(arr);
+
+        arr[0][0] = 1;
+        arr[0][2] = 2;
+        arr[1][0] = 3;
+
+        printArray(arr);
+
+        arr[0][2] = 6;
+        arr[1][1] = 5;
+
+        printArray(arr);
     }
 }
