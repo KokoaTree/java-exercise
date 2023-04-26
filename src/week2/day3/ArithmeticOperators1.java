0343822073712cf711c65e452ea335fdebae6a7f
KokoaTree <4kakao1@daum.net>
2023-04-26 13:53:52 +0900

    feat: 배열 길이, 연산자 실습 - ArrLength 실습 - ArithmeticOperators1 실습

---
+++ src/week2/day3/ArithmeticOperators1.java
@@ -0,0 +1,15 @@
+package week2.day3;
+
+public class ArithmeticOperators1 {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 10;
+
+        System.out.printf("a + b = %d\n", num1 + num2);
+        System.out.printf("a - b = %d\n", num1 - num2);
+        System.out.printf("a * b = %d\n", num1 * num2);
+        System.out.printf("a / b = %d\n", num1 / num2);
+        System.out.printf("a %% b = %d\n", num1 % num2);
+
+    }
+}
---
+++ src/week2/day3/ArrLength.java
@@ -0,0 +1,9 @@
+package week2.day3;
+
+public class ArrLength {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        System.out.println(arr.length);
+    }
+}
