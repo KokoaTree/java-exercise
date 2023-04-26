930e751c215fb147a98ec65d60d82a75fc12d3ea
KokoaTree <4kakao1@daum.net>
2023-04-26 10:35:53 +0900

    feat: 2차원 배열 실습 - TextTwo 실습

---
+++ src/week2/day3/TextTwo.java
@@ -0,0 +1,28 @@
+package week2.day3;
+
+import java.util.Arrays;
+
+public class TextTwo {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+        System.out.println();
+
+        int[][] arr10X10 = new int[10][10];
+
+        System.out.println(Arrays.toString(arr10X10[0]));
+        System.out.println(Arrays.toString(arr10X10[1]));
+        System.out.println(Arrays.toString(arr10X10[2]));
+        System.out.println(Arrays.toString(arr10X10[3]));
+        System.out.println(Arrays.toString(arr10X10[4]));
+        System.out.println(Arrays.toString(arr10X10[5]));
+        System.out.println(Arrays.toString(arr10X10[6]));
+        System.out.println(Arrays.toString(arr10X10[7]));
+        System.out.println(Arrays.toString(arr10X10[8]));
+        System.out.println(Arrays.toString(arr10X10[9]));
+
+    }
+}
