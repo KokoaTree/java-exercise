f9a19ae6a99a2113a5bb2dfdea94651c0103d979
KokoaTree <4kakao1@daum.net>
2023-04-26 14:08:17 +0900

    feat: 몫, 나머지 구하기 - RemainderQuote 실습

---
+++ src/week2/day3/RemainderQuote.java
@@ -0,0 +1,16 @@
+package week2.day3;
+
+public class RemainderQuote {
+    public static void main(String[] args) {
+        int iVal = 1;
+
+        System.out.println(iVal / 2);
+        System.out.println();
+
+        int quote = 10 / 3;
+        int remainder = 10 % 3;
+
+        System.out.printf("10을 3으로 나눈 몫은 %d, 나머지는 %d이다.", quote, remainder);
+
+    }
+}
