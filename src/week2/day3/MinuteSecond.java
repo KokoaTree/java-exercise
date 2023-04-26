889c3fae5fb2732c40d6b5dbac44bca256317ac3
KokoaTree <4kakao1@daum.net>
2023-04-26 15:02:29 +0900

    feat: 시간 분,초 구하기 - MinuteSecond 실습

---
+++ src/week2/day3/MinuteSecond.java
@@ -0,0 +1,10 @@
+package week2.day3;
+
+public class MinuteSecond {
+    public static void main(String[] args) {
+        int seconds = 239;
+        int minute = seconds / 60;
+        int remainSeconds = seconds % 60;
+        System.out.printf("%d분 %d초\n", minute, remainSeconds);
+    }
+}
