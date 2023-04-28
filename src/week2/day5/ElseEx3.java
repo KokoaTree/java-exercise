018958400cd000bfe99733eb41b68c0d63167efa
KokoaTree <4kakao1@daum.net>
2023-04-28 10:22:34 +0900

    feat: 반복문 실습3 - ElseEx3 실습 - GetGrade 실습

---
+++ src/week2/day5/ElseEx3.java
@@ -0,0 +1,14 @@
+package week2.day5;
+
+public class ElseEx3 {
+    public static void main(String[] args) {
+        int score = 81;
+        if (score >= 90) {
+            System.out.println("A");
+        } if (score >= 80) {
+            System.out.println("B");
+        } if (score >= 70) {
+            System.out.println("C");
+        }
+    }
+}
