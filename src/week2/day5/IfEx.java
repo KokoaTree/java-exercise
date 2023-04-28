b6bcc0f615b865208cace0282e4141213f66080c
KokoaTree <4kakao1@daum.net>
2023-04-28 09:10:35 +0900

    feat: 반복문 실습 - IfEx 실습 - IfComparisonOperation 실습

---
+++ src/week2/day5/IfEx.java
@@ -0,0 +1,13 @@
+package week2.day5;
+
+public class IfEx {
+    public static void main(String[] args) {
+
+        if(true){
+            System.out.println("항상 실행됩니다.");
+        }
+        if(false){
+            System.out.println("항상 실행됩니다.");
+        }
+    }
+}
