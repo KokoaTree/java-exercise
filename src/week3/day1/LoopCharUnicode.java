d6422154fae5c3ef772883e8d267ef7aabe0bff3
KokoaTree <4kakao1@daum.net>
2023-05-01 14:19:42 +0900

    feat: LoopCharUnicode, MultiplicationTable - 구구단 2단 실습 - 유니코드 반복 출력

---
+++ src/week3/day1/LoopCharUnicode.java
@@ -0,0 +1,9 @@
+package week3.day1;
+
+public class LoopCharUnicode {
+    public static void main(String[] args) {
+        for(char c = '가'; c < '바'; c++) {
+            System.out.printf("%c ", c);
+        }
+    }
+}
---
+++ src/week3/day1/MultiplicationTable.java
@@ -0,0 +1,13 @@
+package week3.day1;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 9; i++) {
+            System.out.printf("2 X %d = %2d\n", i, i * 2);
+        }
+
+//        for(int i = 1; i <= 9; i++) {
+//            System.out.printf("3 X %d = %2d\n", i, i * 3);
+//        }
+    }
+}
