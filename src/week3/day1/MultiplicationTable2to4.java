c2b0529d141cb749a079c37403f380dfb9be1cce
KokoaTree <4kakao1@daum.net>
2023-05-01 14:49:25 +0900

    feat: MultiplicationTable2to4 - 구구단 2단 ~ 6단 출력(메소드사용)

--- src/week3/day1/MultiplicationTable2to4.java
+++ src/week3/day1/MultiplicationTable2to4.java
@@ -1,13 +1,20 @@
 package week3.day1;
 
 public class MultiplicationTable2to4 {
-
-    public static void main(String[] args) {
-        for(int i = 2; i <= 4; i++) {
-            System.out.println("====" + i + "단====");
-            for(int j = 1; j <= 9; j++) {
-                System.out.printf("%d X %d = %2d\n", i, j, i * j);
+    public static void printDan(int dan) {
+        for (int j = dan; j <= dan; j++) {
+            System.out.printf("====%d단====\n", j);
+            for (int i = 1; i <= 9; i++) {
+                System.out.printf("%d X %d = %2d\n", dan, i, dan * i);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        printDan(2);
+        printDan(3);
+        printDan(4);
+        printDan(5);
+        printDan(6);
     }
 }
